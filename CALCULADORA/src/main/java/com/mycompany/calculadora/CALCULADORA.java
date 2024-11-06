fc96fc3077de23b05dbf0f3853e48ad8fe6381ef
Isabella-dev716 <2isabella4santos21@gmail.com>
2024-11-06 10:41:25 -0300

    Add files via upload

---
+++ CALCULADORA/src/main/java/com/mycompany/calculadora/CALCULADORA.java
@@ -0,0 +1,25 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+import Views.TelaPrincipal;
+
+/**
+ *
+ * @author devmat
+ */
+public class CALCULADORA {
+
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        // deixando a tela visivel
+        tela.setVisible(true);
+    }
+        
+        
+    }
+
+
+    
